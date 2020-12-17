d5227f60fb0e6427835a68a93cf33e4acae74c04
Max <bloodmailvol1@gmail.com>
2020-12-17 19:59:00 +0300

    Создал GoalsModel - модель под задачи и FileIOService - класс для записи/считывания с файла
    Также добавил NewtonSoft.Json из NuGet Packages Manager будем сохранять задачи в Json файл

---
+++ Goals/Goals/Models/GoalsModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goals.Models
+{
+    public class GoalsModel : INotifyPropertyChanged
+    {
+        private string _goalName;
+        private string _goalComment;
+        private bool _isDone;
+        public string goalName
+        {
+            get { return _goalName; }
+            set
+            {
+                if (_goalName == value) return;
+                _goalName = value;
+                OnPropertyChanged("goalName");
+
+            }
+        }
+        public string goalComment
+        {
+            get { return _goalComment; }
+            set
+            {
+                if (_goalComment == value) return;
+                _goalComment = value;
+                OnPropertyChanged("goalComment");
+            }
+        }
+        public bool isDone
+        {
+            get { return _isDone; }
+            set
+            {
+                if (_isDone == value) return;
+                _isDone = value;
+                OnPropertyChanged("isDone");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Goals/Goals/Services/FileIOService.cs
@@ -0,0 +1,47 @@
+﻿using Goals.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goals.Services
+{
+    class FileIOService
+    {
+        private readonly string Path;
+
+        public FileIOService(string path)
+        {
+            Path = path;
+        }
+
+        public BindingList<GoalsModel> LoadData()
+        {
+            var fileExists = File.Exists(Path);
+            if (!fileExists)
+            {
+                File.CreateText(Path).Dispose();
+                return new BindingList<GoalsModel>();
+            }
+
+            using (var reader = File.OpenText(Path))
+            {
+                var fileText = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<BindingList<GoalsModel>>(fileText);
+            }
+        }
+
+        public void SaveData(object saveData)
+        {
+            using (StreamWriter writer = File.CreateText(Path))
+            {
+                string output = JsonConvert.SerializeObject(saveData);
+                writer.Write(output);
+            }
+        }
+    }
+}
