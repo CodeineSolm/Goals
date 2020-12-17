aa3748d8d994fbdedab96de5dccd651a3f7e7e23
Max <bloodmailvol1@gmail.com>
2020-12-17 19:36:05 +0300

    Создал класс MainViewModel и MainView окно для Caliburn Micro
    Теперь запускается MainView как главное окно (пустое окно пока)

--- Goals/Goals/Bootstrapper.cs
+++ Goals/Goals/Bootstrapper.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using Caliburn.Micro;
+using Goals.ViewModels;
 
 namespace Goals
 {
@@ -12,6 +14,10 @@ namespace Goals
         public Bootstrapper()
         {
             Initialize();
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainViewModel>();
         }
     }
 }
