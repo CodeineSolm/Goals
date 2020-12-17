9fc9177f202d140be841587ed51236407d252305
Max <bloodmailvol1@gmail.com>
2020-12-17 20:19:53 +0300

    Добавил ViewModel классы под разные "цели" и UserControl View для этих классов
    +логика в классах

---
+++ Goals/Goals/ViewModels/LongGoalsViewModel.cs
@@ -0,0 +1,66 @@
+﻿using Caliburn.Micro;
+using Goals.Models;
+using Goals.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Goals.ViewModels
+{
+    public class LongGoalsViewModel : Screen
+    {
+        private readonly string GoalsPath = $"{Environment.CurrentDirectory}\\LongGoals.json";
+        private FileIOService _fileIOService;
+
+        private BindingList<GoalsModel> _longGoals;
+        public BindingList<GoalsModel> longGoals
+        {
+            get { return _longGoals; }
+            set
+            {
+                _longGoals = value;
+                NotifyOfPropertyChange(() => longGoals);
+            }
+        }
+
+        public LongGoalsViewModel()
+        {
+            _fileIOService = new FileIOService(GoalsPath);
+            try
+            {
+                longGoals = _fileIOService.LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            if (longGoals == null)
+            {
+                longGoals = new BindingList<GoalsModel>();
+            }
+
+            longGoals.ListChanged += Goals_ListChanged;
+        }
+
+        private void Goals_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            if (e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemDeleted || e.ListChangedType == ListChangedType.ItemChanged)
+            {
+                try
+                {
+                    _fileIOService.SaveData(longGoals);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Goals/Goals/ViewModels/ShortGoalsViewModel.cs
@@ -0,0 +1,66 @@
+﻿using Caliburn.Micro;
+using Goals.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Goals.Services;
+using System.ComponentModel;
+using System.Windows;
+
+namespace Goals.ViewModels
+{
+    public class ShortGoalsViewModel : Screen
+    {
+        private readonly string GoalsPath = $"{Environment.CurrentDirectory}\\ShortGoals.json";
+        private FileIOService _fileIOService;
+
+        private BindingList<GoalsModel> _shortGoals;
+        public BindingList<GoalsModel> shortGoals
+        {
+            get { return _shortGoals; }
+            set
+            {
+                _shortGoals = value;
+                NotifyOfPropertyChange(() => shortGoals);
+            }
+        }
+
+        public ShortGoalsViewModel()
+        {
+            _fileIOService = new FileIOService(GoalsPath);
+            try
+            {
+                shortGoals = _fileIOService.LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            if (shortGoals == null)
+            {
+                shortGoals = new BindingList<GoalsModel>();
+            }
+
+            shortGoals.ListChanged += Goals_ListChanged;
+        }
+
+        private void Goals_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            if (e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemDeleted || e.ListChangedType == ListChangedType.ItemChanged)
+            {
+                try
+                {
+                    _fileIOService.SaveData(shortGoals);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
